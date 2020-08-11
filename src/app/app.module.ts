fa5f78184a0343ad1d281c4e327a05570810556b
mcksyrek <mcksyrek@gmail.com>
2020-08-11 23:02:59 +0200

    formsModule moved into lazyloaded module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/new-service/new-service.module.ts
+++ src/app/new-service/new-service.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { NewServiceRoutingModule } from './new-service-routing.module';
 import { NewServiceComponent } from './new-service/new-service.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [NewServiceComponent],
-  imports: [CommonModule, NewServiceRoutingModule],
+  imports: [
+    CommonModule,
+    NewServiceRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
 })
 export class NewServiceModule {}
