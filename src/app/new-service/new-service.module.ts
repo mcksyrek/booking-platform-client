c0a5447504dfb5d2133fb15c4dbe933e68fa1287
mcksyrek <mcksyrek@gmail.com>
2020-08-16 11:20:23 +0200

    moved state to feature module scope

--- src/app/new-service/new-service.module.ts
+++ src/app/new-service/new-service.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { NewServiceRoutingModule } from './new-service-routing.module';
 import { NewServiceComponent } from './new-service/new-service.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgxsModule } from '@ngxs/store';
+
+import { ServicesState } from '@app/store/state/services.state';
 
 @NgModule({
   declarations: [NewServiceComponent],
@@ -12,6 +15,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     NewServiceRoutingModule,
     ReactiveFormsModule,
     FormsModule,
+    NgxsModule.forRoot([ServicesState]),
   ],
 })
 export class NewServiceModule {}
