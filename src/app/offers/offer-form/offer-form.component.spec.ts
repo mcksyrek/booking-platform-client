64776de7932ee38f4001721879e058ba5057c7b0
mcksyrek <mcksyrek@gmail.com>
2020-08-21 01:08:43 +0200

    [feat] tests prepared for curr state of app

--- src/app/offers/offer-form/offer-form.component.spec.ts
+++ src/app/offers/offer-form/offer-form.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { OfferFormComponent } from './offer-form.component';
 
@@ -9,6 +10,7 @@ describe('OfferFormComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [OfferFormComponent],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   }));
 
--- src/app/offers/offers-list/offers-list.component.spec.ts
+++ src/app/offers/offers-list/offers-list.component.spec.ts
@@ -1,20 +1,33 @@
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { OffersListComponent } from './offers-list.component';
+import { OffersState } from '../state/offers.state';
+
+@Component({
+  selector: 'booking-offer-form',
+  template: '<p>Mock Product Editor Component</p>',
+})
+class MockOfferFormComponent {}
 
 describe('OffersListComponent', () => {
   let component: OffersListComponent;
   let fixture: ComponentFixture<OffersListComponent>;
-
+  let store: Store;
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [OffersListComponent],
+      declarations: [OffersListComponent, MockOfferFormComponent],
+      imports: [ReactiveFormsModule, NgxsModule.forRoot([OffersState])],
     }).compileComponents();
+    store = TestBed.inject(Store);
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OffersListComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store);
     fixture.detectChanges();
   });
 
