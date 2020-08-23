3aaff15fd1f6a35a2eb5ea2aa3f583990f716f9e
mcksyrek <mcksyrek@gmail.com>
2020-08-23 19:28:17 +0200

    test(offer): testing offer-list

--- src/app/offers/offers-list/offers-list.component.spec.ts
+++ src/app/offers/offers-list/offers-list.component.spec.ts
@@ -1,25 +1,22 @@
-import { Component } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxsModule, Store } from '@ngxs/store';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { OffersListComponent } from './offers-list.component';
 import { OffersState } from '../state/offers.state';
-
-@Component({
-  selector: 'booking-offer-form',
-  template: '<p>Mock Product Editor Component</p>',
-})
-class MockOfferFormComponent {}
+import { AddOfferAction } from '../state/offers.actions';
 
 describe('OffersListComponent', () => {
   let component: OffersListComponent;
   let fixture: ComponentFixture<OffersListComponent>;
   let store: Store;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [OffersListComponent, MockOfferFormComponent],
+      declarations: [OffersListComponent],
       imports: [ReactiveFormsModule, NgxsModule.forRoot([OffersState])],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
     store = TestBed.inject(Store);
   }));
@@ -33,5 +30,18 @@ describe('OffersListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(' toggleOfferForm should toggle `showOfferForm` ', () => {
+    component.toggleOfferForm();
+    expect(component.showOfferForm).toBeTruthy();
+  });
+
+  it('should access offers from Store', () => {
+    const mockOffer = { offerName: '123' };
+    store.dispatch(new AddOfferAction(mockOffer));
+
+    const offers = store.selectSnapshot(state => [...state.offers.offers]);
+    expect(offers.find(offer => offer === mockOffer)).toBeTruthy();
   });
 });
