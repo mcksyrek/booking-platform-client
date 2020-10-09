52ec110cacfd09548a38e07caacc88709728702c
mcksyrek <mcksyrek@gmail.com>
2020-10-09 15:25:26 +0200

    test: getOffersList tested

--- src/app/offers/offers.service.spec.ts
+++ src/app/offers/offers.service.spec.ts
@@ -1,16 +1,46 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { OffersService } from './offers.service';
+import { IOffer } from './offer.interface';
+import { Endpoints } from '@booking/shared/constants';
 
 describe('OffersService', () => {
   let service: OffersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [OffersService],
+    });
+
+    httpMock = TestBed.inject(HttpTestingController);
     service = TestBed.inject(OffersService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('#getOffersList', () => {
+    it('should return an Observable<IOffer[]>', () => {
+      const mockResponse: IOffer[] = [
+        { name: 'mockName1', id: 'mockID1' },
+        { name: 'mockName2', id: 'mockID2' },
+      ];
+
+      service.getOffersList().subscribe(offers => {
+        expect(offers.length).toBe(2);
+        expect(offers).toEqual(mockResponse);
+      });
+
+      const req = httpMock.expectOne(Endpoints.Offers);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockResponse);
+    });
   });
 });
