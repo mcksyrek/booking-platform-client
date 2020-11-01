94f1d14e9c274b90ee488a8a8efdf03935b9f990
mcksyrek <mcksyrek@gmail.com>
2020-11-01 23:00:12 +0100

    feat: added type to MENU_LIST_ITEMS

--- src/app/layout/menu/menu.constant.ts
+++ src/app/layout/menu/menu.constant.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@booking/shared/enums/routes.enum';
 
-export const MENU_LIST_ITEMS = [
+interface IMenuItem {
+  name: string;
+  route: string;
+}
+
+export const MENU_LIST_ITEMS: IMenuItem[] = [
   { name: 'Browse offers', route: Routes.Offers + Routes.All },
   { name: 'Create new offer', route: Routes.Offers + Routes.New },
 ];
