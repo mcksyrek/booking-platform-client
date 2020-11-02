f08154515b1ace4b243d24606c977dbe55aafff3
mcksyrek <mcksyrek@gmail.com>
2020-11-02 21:31:09 +0100

    feat: combined filters

--- src/app/offers/toolbar/toolbar.component.ts
+++ src/app/offers/toolbar/toolbar.component.ts
@@ -8,7 +8,7 @@ import {
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
-import { filter, skip } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 
 import { AbstractSubscriber } from '@booking/shared/classes/abstract-subscriber';
 import { SortingTypesEnum } from '@booking/shared/enums';
@@ -24,14 +24,14 @@ import { filterOffersByAttribute } from '@booking/shared/utils';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToolbarComponent extends AbstractSubscriber implements OnInit {
+  // TODO change architecture to use Store
   @Output() setOffersList = new EventEmitter<IOffer[]>();
   @Select(OffersState.getOffers)
   readonly allOffers$: Observable<IOffer[]>;
   readonly sortingTypes = Object.values(SortingTypesEnum);
+
   readonly toolbarForm: FormGroup;
   readonly filtersGroup: FormGroup;
-  readonly categoriesFilter: FormControl;
-  readonly citiesFilter: FormControl;
 
   allOffersList: IOffer[];
   allCategoriesList: string[];
@@ -42,41 +42,27 @@ export class ToolbarComponent extends AbstractSubscriber implements OnInit {
     this.toolbarForm = _formBuilder.group({
       sort: [this.sortingTypes[0]],
       filters: this._formBuilder.group({
-        categories: [],
-        cities: [],
+        category: [],
+        city: [],
       }),
     });
 
     this.filtersGroup = this.toolbarForm.controls.filters as FormGroup;
-    this.categoriesFilter = this.filtersGroup.controls
-      .categories as FormControl;
-    this.citiesFilter = this.filtersGroup.controls.cities as FormControl;
   }
 
   ngOnInit(): void {
     this.addSubscriptions([
       this.allOffers$.pipe(filter(offers => !!offers)).subscribe(offers => {
         this.allOffersList = sortOffers(this.toolbarForm.value.sort, offers);
-
-        this.allCategoriesList = this._createArrOfUniqueValues(
-          offers,
-          'category'
-        );
-        this.categoriesFilter.patchValue(this.allCategoriesList);
-
-        this.allCitiesList = this._createArrOfUniqueValues(offers, 'city');
-        this.citiesFilter.patchValue(this.allCitiesList);
+        this._setUniqueFilters(offers);
       }),
 
       this.toolbarForm.controls.sort.valueChanges.subscribe(sortType =>
-        this.setOffersList.emit(sortOffers(sortType, this.allOffersList))
+        this.setOffersList.emit(sortOffers(sortType, this._combinedFilters()))
       ),
 
-      this.categoriesFilter.valueChanges.subscribe(() =>
-        this.setOffersList.emit(this._combineOfferFilters())
-      ),
-      this.citiesFilter.valueChanges.subscribe(() =>
-        this.setOffersList.emit(this._combineOfferFilters())
+      this.filtersGroup.valueChanges.subscribe(() =>
+        this.setOffersList.emit(this._combinedFilters())
       ),
     ]);
   }
@@ -91,32 +77,25 @@ export class ToolbarComponent extends AbstractSubscriber implements OnInit {
 
     return Array.from(valuesSet);
   }
-  private _combineOfferFilters(): IOffer[] {
-    // console.log(Object.keys(this.filtersGroup.value));
-    // return Object.keys(this.filtersGroup.value).reduce(
-    //   (filteredOffers, filterType) => {
-    //     // console.log(this.filtersGroup.value[filterType]);
-    //     // debugger;
-    //     const temp = filterOffersByAttribute(
-    //       filterType,
-    //       this.filtersGroup.value[filterType],
-    //       filteredOffers
-    //     );
-    //     console.log(temp);
-    //     return temp;
-    //   },
-    //   this.allOffersList
-    // );
-    const filteredByCategories = filterOffersByAttribute(
-      'category',
-      this.categoriesFilter.value,
+
+  private _combinedFilters(): IOffer[] {
+    return Object.keys(this.filtersGroup.value).reduce(
+      (filteredOffers, filterType) => {
+        return filterOffersByAttribute(
+          filterType,
+          this.filtersGroup.value[filterType],
+          filteredOffers
+        );
+      },
       this.allOffersList
     );
+  }
 
-    return filterOffersByAttribute(
-      'city',
-      this.citiesFilter.value,
-      filteredByCategories
-    );
+  private _setUniqueFilters(offers: IOffer[]): void {
+    this.allCategoriesList = this._createArrOfUniqueValues(offers, 'category');
+    this.filtersGroup.controls.category.patchValue(this.allCategoriesList);
+
+    this.allCitiesList = this._createArrOfUniqueValues(offers, 'city');
+    this.filtersGroup.controls.city.patchValue(this.allCitiesList);
   }
 }
