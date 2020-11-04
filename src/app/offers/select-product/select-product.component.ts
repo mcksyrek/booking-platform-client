2c6b839c11c53db2ea50adf1dcfb71b4c5e02e7c
mcksyrek <mcksyrek@gmail.com>
2020-11-04 13:09:22 +0100

    feat: filtering avaliable hours for offer

--- src/app/offers/select-product/select-product.component.ts
+++ src/app/offers/select-product/select-product.component.ts
@@ -9,8 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { IProduct } from '../offer.interface';
 import { AbstractSubscriber } from '@booking/shared/classes/abstract-subscriber';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { MOCK_SERVER_RES, STATIC_AVALIABLE_HOURS } from './constants';
 
 @Component({
   selector: 'booking-select-product',
@@ -22,20 +23,10 @@ export class SelectProductComponent extends AbstractSubscriber
   implements OnInit {
   readonly product: IProduct;
   readonly selectDateForm: FormGroup;
+  readonly staticAvaliableHours = STATIC_AVALIABLE_HOURS;
+  readonly bookedHours = MOCK_SERVER_RES;
+
   avaliableHours: string[];
-  // TODO move it to constants
-  readonly staticAvaliableHours = [
-    '8:00',
-    '9:00',
-    '10:00',
-    '11:00',
-    '12:00',
-    '13:00',
-    '14:00',
-    '15:00',
-    '16:00',
-  ];
-  readonly mockServerRes = ['9:00', '10:00', '11:00', '13:00', '15:00'];
 
   get disableSubmit(): boolean {
     return !this.selectDateForm.valid;
@@ -59,19 +50,55 @@ export class SelectProductComponent extends AbstractSubscriber
         .pipe(
           switchMap(selectedDate => {
             // TODO HTTPReq here
-            return of(this.mockServerRes);
-          })
+            return of(this.bookedHours);
+          }),
+          map(bookedHours => new Set(bookedHours))
         )
-        .subscribe(serverRes => {
-          this.avaliableHours = this.staticAvaliableHours.filter(
-            hour => !serverRes.includes(hour)
+        .subscribe(bookedHoursSet => {
+          const filteredHours = this.staticAvaliableHours.filter(
+            hour => !bookedHoursSet.has(hour)
           );
-          console.log(this.avaliableHours);
+          this.avaliableHours = this._checkHoursDuration(
+            this.product.duration,
+            filteredHours
+          );
         })
     );
   }
 
   selectHour(hour: string): void {
     this.selectDateForm.controls.selectedHour.setValue(hour);
+  }
+
+  private _checkHoursDuration(
+    duration: number,
+    hoursToCheck: number[]
+  ): string[] {
+    // O(1)
+    const hoursToCheckLength = hoursToCheck.length;
+    // O(n)
+    const hoursToCheckSet = new Set(hoursToCheck);
+
+    // O(n)
+    return hoursToCheck.reduce((avaliableHours, hourToCheck, index) => {
+      // best case O(1)
+      if (hoursToCheckLength - index - duration < 0) {
+        return avaliableHours;
+      }
+      // worst case - depends on duration so O(m)
+      for (let i = 0; i < duration; i++) {
+        // O(1)
+        if (!hoursToCheckSet.has(hourToCheck + i)) {
+          return avaliableHours;
+        }
+      }
+      return [...avaliableHours, this._mapNumberToHourString(hourToCheck)];
+    }, []);
+    // total complexity: 0(1)+0(n)+0(n)*0(1 or m) => so best 0(2n) and worst O(n(1+m))
+    // fair enough?
+  }
+
+  private _mapNumberToHourString(hour: number): string {
+    return `${hour}:00`;
   }
 }
